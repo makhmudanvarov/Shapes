07ebc87f1f25ce586b197ef0e9a97a76be654917
Makhmud <fordailyuse0108@gmail.com>
2025-09-24 09:25:13 +0500

    color change done

--- shapes.js
+++ shapes.js
@@ -1,30 +1,38 @@
-const shapes = document.querySelectorAll('.shape');
+const btn_shape = document.querySelectorAll(".btn-shape");
+const colors = document.querySelectorAll(".color");
+const shapes = document.querySelectorAll(".shape");
+console.log(shapes)
 
-const array = ['red', 'green', 'blue', 'grey', 'indigo', 'aqua', 'pink', 'black'];
+// const array = ['red', 'green', 'blue', 'grey', 'indigo', 'aqua', 'pink', 'black'];
 
-function rand() {
-    let i = Math.floor(Math.random() * array.length);
-    return array[i];
-}
+// function rand() {
+//     let i = Math.floor(Math.random() * array.length);
+//     return array[i];
+// }
 
-shapes.forEach(shape => {
-    shape.addEventListener('click', () => {
-        shape.style = `background: ${rand()};
-        -webkit-clip-path: polygon(
-        25% 0%,
-        75% 0%,
-        100% 50%,
-        75% 100%,
-        25% 100%,
-        0% 50%
-      );
-      clip-path: polygon(
-        25% 0%,
-        75% 0%,
-        100% 50%,
-        75% 100%,
-        25% 100%,
-        0% 50%
-      );`
+colors.forEach(color => {
+  color.addEventListener('click', () => {
+    const selectColor = color.classList[1];
+
+    shapes.forEach(shape => {
+      shape.style.background = selectColor;
     })
-})
+  })
+});
+
+btn_shape.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const selectColor = btn.classList[1];
+
+    shapes.forEach(shape => {
+      shape.style.background = selectColor;
+    })
+  })
+});
+
+
+// shapes.forEach(shape => {
+//     shape.addEventListener('click', () => {
+//         shape.style = `background: ${rand()};`
+//     })
+// })
